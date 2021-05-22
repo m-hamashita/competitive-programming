41d5b123d6f13727a7ed0d49a70f876a19d7de26
m.hama <masakatsu.hamashita@gunosy.com>
2021-05-22 14:40:12 +0900

    dp_D

--- dp/D/main.cpp
+++ dp/D/main.cpp
@@ -11,12 +11,12 @@ signed main() {
   cin >> N >> W;
   int w[N], v[N];
   rep(i, N) cin >> w[i] >> v[i];
-  int value[W+1];
+  int value[W + 1];
   rep(i, W + 1) value[i] = 0;
 
   for (int i = 0; i < N; i++) {
     for (int j = W; j >= w[i]; j--) {
-      value[j] = max(value[j], value[j-w[i]]+v[i]);
+      value[j] = max(value[j], value[j - w[i]] + v[i]);
     }
   }
   cout << value[W] << endl;
