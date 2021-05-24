5a867c7eb2c95e62c28e4be750dffc13923a30cf
m.hama <masakatsu.hamashita@gunosy.com>
2021-05-24 23:51:41 +0900

    Add utils.cpp

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <vector>
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
@@ -29,45 +29,50 @@ long long pow_mod(long long x, long long y, long long MOD) {
 long long modinv(long long a, long long m) {
   long long b = m, u = 1, v = 0;
   while (b) {
-      long long t = a / b;
-      a -= t * b; swap(a, b);
-      u -= t * v; swap(u, v);
+    long long t = a / b;
+    a -= t * b;
+    swap(a, b);
+    u -= t * v;
+    swap(u, v);
   }
   u %= m;
-  if (u < 0) u += m;
+  if (u < 0)
+    u += m;
   return u;
 }
 
 long long conbination(long long n, long long r, int MOD) {
   long long fact_r = 1;
-  for (int i=1;i<=r;i++){
+  for (int i = 1; i <= r; i++) {
     fact_r *= i;
     fact_r = fact_r % MOD;
   }
   // fact_r の逆元を求める
   // long long result = pow_mod(fact_r, MOD-2, MOD);
   long long result = modinv(fact_r, MOD);
-  for(int i=n-r+1;i<=n;i++){
-    result = (result * i)%MOD;
+  for (int i = n - r + 1; i <= n; i++) {
+    result = (result * i) % MOD;
   }
   return result;
 }
 void COMinit() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < MAX; i++){
-        fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
-        finv[i] = finv[i - 1] * inv[i] % MOD;
-    }
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < MAX; i++) {
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
 }
 
 // 二項係数計算
-long long COM(int n, int k){
-    if (n < k) return 0;
-    if (n < 0 || k < 0) return 0;
-    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+long long COM(int n, int k) {
+  if (n < k)
+    return 0;
+  if (n < 0 || k < 0)
+    return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
 // UnionFind Tree
@@ -75,33 +80,33 @@ struct UnionFind {
   vector<int> par, siz;
 
   // initialize
-  UnionFind(int n): par(n, -1), siz(n, 1) {}
+  UnionFind(int n) : par(n, -1), siz(n, 1) {}
 
-  int root(int x){
-    if (par[x] == -1) return x;
-    else return par[x] = root(par[x]);
+  int root(int x) {
+    if (par[x] == -1)
+      return x;
+    else
+      return par[x] = root(par[x]);
   }
 
-  int issame(int x, int y){
-    return root(x)==root(y);
-  }
+  int issame(int x, int y) { return root(x) == root(y); }
 
-  bool unite(int x, int y){
+  bool unite(int x, int y) {
     x = root(x);
     y = root(y);
-    if (x == y) return false;
-    if (siz[x] < siz[y]) swap(x, y);
+    if (x == y)
+      return false;
+    if (siz[x] < siz[y])
+      swap(x, y);
     par[y] = x;
     siz[x] += siz[y];
     return true;
   }
 
-  int size(int x){
-    return siz[root(x)];
-  }
+  int size(int x) { return siz[root(x)]; }
 };
 
-signed main(){
+signed main() {
   cout << 1999999 << "'s inv: " << modinv(1999999, MOD) << endl;
   cout << 1999999 * modinv(1999999, MOD) % MOD << endl;
   return 0;
