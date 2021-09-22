828f7919b3bae8fe3fea0096629e8ea08cd3a435
m-hamashita <masakatsu.hamashita@gunosy.com>
2021-09-22 10:00:28 +0900

    ABC219 B

--- ABC219/B/main.cpp
+++ ABC219/B/main.cpp
@@ -7,8 +7,21 @@ const long long INF = 1LL << 60;
 using namespace std;
 
 signed main() {
-    ll N;
-    cin >> N;
+    string S1, S2, S3, T;
+    cin >> S1 >> S2 >> S3 >> T;
+    string result = "";
+    for (int i = 0; i < T.size(); i++) {
+        if (T[i] == '1') {
+            result += S1;
+        }
+        if (T[i] == '2') {
+            result += S2;
+        }
+        if (T[i] == '3') {
+            result += S3;
+        }
+    }
+    cout << result << endl;
 
     return 0;
 }
