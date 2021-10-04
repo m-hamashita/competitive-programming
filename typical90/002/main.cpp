6a07014876bb08e81266eb7ed855154a26d6f7ec
m-hamashita <masakatsu.hamashita@gunosy.com>
2021-10-04 21:06:13 +0900

    small fix

--- typical90/002/main.cpp
+++ typical90/002/main.cpp
@@ -3,33 +3,33 @@
 
 using namespace std;
 bool check(string s) {
-  stack<char> st;
-  for (int i = 0; i < s.length(); i++) {
-    if (s[i] == '(')
-      st.push(s[i]);
-    else {
-      if (st.empty() || st.top() != '(')
-        return false;
-      st.pop();
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(')
+            st.push(s[i]);
+        else {
+            if (st.empty() || st.top() != '(')
+                return false;
+            st.pop();
+        }
     }
-  }
-  return st.empty();
+    return st.empty();
 }
 
 signed main() {
-  int N;
-  cin >> N;
-  for (int i = 0; i < (1 << N); i++) {
-    string s = "";
-    for (int j = N - 1; j >= 0; j--) {
-      if (i & (1 << j))
-        s += ')';
-      else
-        s += '(';
+    int N = 4;
+    // cin >> N;
+    for (int i = 0; i < (1 << N); i++) {
+        string s = "";
+        for (int j = N - 1; j >= 0; j--) {
+            if (i & (1 << j))
+                s += ')';
+            else
+                s += '(';
+        }
+        if (check(s))
+            cout << s << endl;
     }
-    if (check(s))
-      cout << s << endl;
-  }
 
-  return 0;
+    return 0;
 }
