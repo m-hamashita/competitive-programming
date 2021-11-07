e52f88d342ff7722da74c8725993794abedbbe24
m-hamashita <masakatsu.hamashita@gunosy.com>
2021-11-07 23:17:16 +0900

    ABC226 A

---
+++ ABC226/A/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+const long long INF = 1LL << 60;
+
+using namespace std;
+
+signed main() {
+    float N;
+    cin >> N;
+    cout << int(N + 0.5) << endl;
+
+    return 0;
+}
