bff197ba209e93dcd709c8415def8e830367320b
m-hamashita <masakatsu.hamashita@gunosy.com>
2021-12-11 23:31:26 +0900

    ABC231 A

---
+++ ABC231/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+const long long INF = 1LL << 60;
+
+using namespace std;
+
+signed main() {
+    double N;
+    cin >> N;
+    cout << N / 100.0 << endl;
+
+    return 0;
+}
