c57c7108e5cfdd4df8f1fe42361a969eae317256
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-12-29 11:12:00 +0900

    Update template file

--- template.cpp
+++ template.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 
+#include <atcoder/all>
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define ll long long
 const long long INF = 1LL << 60;
 
 using namespace std;
+using namespace atcoder;
 
 signed main() {
     ll N;
