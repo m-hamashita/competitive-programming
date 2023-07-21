286dc84a72007c6ff29508af3f729a3b3f00e7a5
m-hamashita <masakatsu.hamashita@gunosy.com>
2023-07-22 00:58:34 +0900

    Conquer

--- leetcode/4.median-of-two-sorted-arrays.rs
+++ leetcode/4.median-of-two-sorted-arrays.rs
@@ -5,17 +5,50 @@
  */
 
 // @lc code=start
+
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        let mut nums = nums1;
-        nums.extend(nums2);
-        nums.sort();
+        let len1 = nums1.len();
+        let len2 = nums2.len();
+        let mut index1 = 0;
+        let mut index2 = 0;
+        let mut index = 0;
+        let mut is_even = false;
+        let mut num = 0;
+        let mut sum = 0;
+        let mut target_index = Vec::new();
 
-        let len = nums.len();
-        if len % 2 == 0 {
-            (nums[len / 2 - 1] + nums[len / 2]) as f64 / 2.0
+        is_even = (len1 + len2) % 2 == 0;
+        if is_even {
+            target_index.push((len1 + len2) / 2 - 1);
+            target_index.push((len1 + len2) / 2);
         } else {
-            nums[len / 2] as f64
+            target_index.push((len1 + len2) / 2);
+        }
+
+        while index1 < len1 || index2 < len2 {
+            if index1 == len1 {
+                num = nums2[index2];
+                index2 += 1;
+            } else if index2 == len2 {
+                num = nums1[index1];
+                index1 += 1;
+            } else if nums1[index1] < nums2[index2] {
+                num = nums1[index1];
+                index1 += 1;
+            } else {
+                num = nums2[index2];
+                index2 += 1;
+            }
+            if target_index.contains(&index) {
+                sum += num;
+            }
+            index += 1;
+        }
+        if is_even {
+            sum as f64 / 2.0
+        } else {
+            sum as f64
         }
     }
 }
